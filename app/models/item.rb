eeabeb97df34dff974e10d2ab998bf26fabdf961
gaki-h <walter.thi.gks@gmail.com>
2022-02-14 19:46:52 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
-  # has_many :users
+  has_many :users
   has_many :orders
   has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_days
+
+
+  validates :title, :text, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_days_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end
