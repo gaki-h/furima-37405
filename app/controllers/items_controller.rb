9ca94b857b0b3edd5f318bec9f13f504ef4a6f07
gaki-h <walter.thi.gks@gmail.com>
2022-02-17 18:15:19 +0900

    ルーティング遷移、エラーメッセージ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, except: [:index]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,9 +24,35 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+    redirect_to item_path
+    else
+    render :edit
+    end
+  end
+
+
+
   private
 
   def item_params
     params.require(:item).permit(:category_id, :condition_id, :shipping_cost_id, :prefecture_id, :image, :name, :description, :shipping_days_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? == current_user
+      redirect_to action: :index
+    end
   end
 end
