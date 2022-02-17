678bb7e5bce50f6db68cf98c9e75d41e00c0b135
gaki-h <walter.thi.gks@gmail.com>
2022-02-17 18:45:50 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :update, :show]
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -51,8 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? == current_user
-      redirect_to action: :index
-    end
+    redirect_to root_path unless current_user == @item.user
   end
 end
