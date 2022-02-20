ad3f38eedc12f080061b28bfee7ca7b18e1e1be8
gaki-h <walter.thi.gks@gmail.com>
2022-02-21 01:04:02 +0900

    確認1

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,21 @@
+// window.addEventListener('load', () => {
+//   const itemPrice = document.getElementById("item-price");
+//   itemPrice.addEventListener("input", () => {
+//     const inputValue = itemPrice.value;
+//     const addTaxDom = document.getElementById("add-tax-price");
+//     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+//     const addProfitDom = document.getElementById("profit");
+//     addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))  
+//  })
+// });
+// 変更差分確認
+
 window.addEventListener('load', () => {
-  const itemPrice = document.getElementById("item-price");
-  itemPrice.addEventListener("input", () => {
-    const inputValue = itemPrice.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
-    const addProfitDom = document.getElementById("profit");
-    addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))  
+  const priceInput = document.getElementById("item-price");
+   priceInput.addEventListener("input", () => {
+     const addTaxDom = document.getElementById("add-tax-price");
+     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+     const addProfitDom = document.getElementById("profit");
+     addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
 });
