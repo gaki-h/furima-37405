6360569ca51fdf3166e94a8dfc11f44dfc4ce287
gaki-h <walter.thi.gks@gmail.com>
2022-02-21 18:00:39 +0900

    エラーメッセージ日本語化

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -5,11 +5,11 @@ class OrderDestination
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフン(-)を入れて入力してください"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :municipalities
     validates :address
-    validates :phone_number, format: { with: /\A\d{11}\z/, message: 'Input only number' }
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: 'は数字のみで入力してください' }
     validates :token
   end
 
